0f43a76dd196ba1e6c8943ebf7bce4b3e4f51372
Nikolay <nik_ses@inbox.ru>
2022-04-17 12:45:15 +0300

    Example C#_1

---
+++ Example_1/Program.cs
@@ -0,0 +1,4 @@
+﻿// Выводим последнюю цифру трехзначного числа 
+Console.WriteLine("Введите трехзначное число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Последняя цифра "+ a % 10);
---
+++ Example_5/Program.cs
@@ -0,0 +1,21 @@
+﻿// показать все четные числа от 1 до N
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+    if (a == 1)
+    {
+        Console.Write("NULL");
+    }
+while (i <= a)
+
+    {
+    
+    if ((i % 2) == 0)
+    {
+    Console.Write(i + ", ");
+    
+    }
+    i= (i+1);
+
+}
+
